8890c98af65ad150e0b2004172d3705770169425
陈焱 <alberteinstein007@126.com>
2021-12-10 16:35:14 +0800

    feat: add bloom effect

--- unknow-effect/index.js
+++ unknow-effect/index.js
@@ -1,8 +1,15 @@
 import { ConvexGeometry } from "../core/ConvexGeometry.js";
 import { OrbitControls } from "../core/OrbitControls.js";
 import * as THREE from "../core/three.module.js";
-
-let scene, camera, renderer, canvas, control, stats, allObjects;
+import { RenderPass } from "../three/examples/jsm/postprocessing/RenderPass.js";
+import { DotScreenPass } from "../three/examples/jsm/postprocessing/DotScreenPass.js";
+import { EffectComposer } from "../three/examples/jsm/postprocessing/EffectComposer.js";
+import { UnrealBloomPass } from "../three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { ClearMaskPass, MaskPass } from "../three/examples/jsm/postprocessing/MaskPass.js";
+import { ShaderPass } from "../three/examples/jsm/postprocessing/ShaderPass.js";
+import { CopyShader } from "../three/examples/jsm/shaders/CopyShader.js";
+import { ColorifyShader } from "../three/examples/jsm/shaders/ColorifyShader.js";
+let scene, camera, renderer, canvas, control, stats, allObjects, obj, effectComposer;
 
 stats = new Stats();
 stats.showPanel(0);
@@ -36,155 +43,97 @@ function main() {
 
     control = new OrbitControls(camera, canvas);
 
-    scene.add(new THREE.AxesHelper(500));
+    // scene.add(new THREE.AxesHelper(500));
 
-    const points = createPoints(40, 1);
+    const points = createPoints(4, 1);
 
-    const BGeo = new ConvexGeometry(points);
+    const size = 1;
+    const BGeo = new THREE.BoxBufferGeometry(size, size, size);
     const BMat = new THREE.MeshPhongMaterial({
-        color: 0xff00f0,
+        color: new THREE.Color("#83a5b9"),
         transparent: true,
-        opacity: 1.0
+        opacity: 1.0,
+        flatShading: true,
+        reflectivity: 0.4,
+        shininess: 80,
     });
 
-    const B = new THREE.Mesh(BGeo, BMat);
+    
 
+   
+    
 
-    const light = new THREE.DirectionalLight({
-        color: 0xff0f0f,
-        intensity: 1.2
-    });
+    const MAXCOUNT = 1000;
 
-    light.position.set(500, 500, 500);
+    obj = new THREE.Object3D();
+    scene.add(obj);
 
-    scene.add(light);
-    scene.add(new THREE.AmbientLight(0xffffff, 0.2));
-    const INSTANCECOUNT = 100;
-    const instanceGeo = new THREE.InstancedBufferGeometry().copy(BGeo);
-    instanceGeo.instanceCount = INSTANCECOUNT;
+    for( let i=0; i<MAXCOUNT; i++) {
+        const B = new THREE.Mesh(BGeo, BMat);
+        B.position.set(Math.random() - 0.5, Math.random() - 0.5,Math.random() - 0.5 ).normalize();
+        B.position.multiplyScalar(Math.random() * 500);
+        B.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2)
+        B.scale.x = B.scale.y = B.scale.z = Math.random();
 
-    let a_position = [], a_scale = [], a_color = [];
-    for(let i=0; i<INSTANCECOUNT; i++) {
-        a_position.push(
-            Math.random() * 100 - 100 / 5,
-            Math.random() * 100 - 100 / 5,
-            Math.random() * 100 - 100 / 5
-        );
-
-        a_color.push( Math.random(),  Math.random(), Math.random());
-
+        obj.add(B);
     }
 
-    instanceGeo.setAttribute("a_position", new THREE.InstancedBufferAttribute(
-        new Float32Array(a_position),
-        3,
-        false 
-    ));
+    const lightDistance = 200;
+    const sceneSun = new THREE.Scene();
+    sceneSun.add(camera);
+    const sunGeo = new THREE.SphereBufferGeometry(20);
+    const sunMat = new THREE.MeshBasicMaterial({color: 0xff0000})
+    const sun = new THREE.Mesh(sunGeo, sunMat);
+    sun.position.set(lightDistance, lightDistance, lightDistance);
+    sceneSun.add(sun);
 
-    instanceGeo.setAttribute("a_color", new THREE.InstancedBufferAttribute(
-        new Float32Array(a_color),
-        3,
-        false
-    ));
-
-    const frag = `
-        varying vec3 v_color;
-        void main() {
-            gl_FragColor = vec4(v_color, 1.0);
-        }
-    `;
-
-    const vertex = `
-        attribute vec3 a_position;
-        attribute vec3 a_color;
-        varying vec3 v_color;
-
-        void main() {
-            vec3 transform = position.xyz;
-
-            transform += a_position;
-
-            v_color = a_color;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(transform, 1.0);
-        }
-    `;
-
-    const vertex1 = `           
-        #define PHONG
-
-        varying vec3 vViewPosition;
-        attribute vec3 a_position;
-        uniform float u_time;
-
-        #include <common>
-        #include <uv_pars_vertex>
-        #include <uv2_pars_vertex>
-        #include <displacementmap_pars_vertex>
-        #include <envmap_pars_vertex>
-        #include <color_pars_vertex>
-        #include <fog_pars_vertex>
-        #include <normal_pars_vertex>
-        #include <morphtarget_pars_vertex>
-        #include <skinning_pars_vertex>
-        #include <shadowmap_pars_vertex>
-        #include <logdepthbuf_pars_vertex>
-        #include <clipping_planes_pars_vertex>
-
-        void main() {
-
-            #include <uv_vertex>
-            #include <uv2_vertex>
-            #include <color_vertex>
-
-            #include <beginnormal_vertex>
-            #include <morphnormal_vertex>
-            #include <skinbase_vertex>
-            #include <skinnormal_vertex>
-            #include <defaultnormal_vertex>
-            #include <normal_vertex>
-
-            #include <begin_vertex>
-            #include <morphtarget_vertex>
-            #include <skinning_vertex>
-            #include <displacementmap_vertex>
-            #include <project_vertex>
-            #include <logdepthbuf_vertex>
-            #include <clipping_planes_vertex>
-
-            vViewPosition = - mvPosition.xyz;
-
-            gl_Position.x += a_position.x + sin(u_time * 0.002);
-            gl_Position.y += a_position.y;
-            gl_Position.z += a_position.z;
-
-            #include <worldpos_vertex>
-            #include <envmap_vertex>
-            #include <shadowmap_vertex>
-            #include <fog_vertex>
-
-        }
-    `;
-
-    const instanceMat = new THREE.ShaderMaterial({
-        fragmentShader: THREE.ShaderLib.phong.fragmentShader,
-        vertexShader: vertex1,
-        uniforms: THREE.UniformsUtils.merge( [
-            THREE.ShaderLib.phong.uniforms,
-            {u_time: THREE.Uniform[0]}
-        ]),
-        lights: true
+    const light = new THREE.DirectionalLight({
+        color: 0xffffff,
     });
 
-    allObjects = new THREE.Mesh(instanceGeo, instanceMat);
+    light.position.set(lightDistance, lightDistance, lightDistance);
+    scene.add(light);
 
-    scene.add(allObjects);
+    // const dotMask = 
+    const randerPass = new RenderPass(scene, camera);
+    // randerPass.clear = false;
+    const renderPass2 = new RenderPass(sceneSun, camera);
+    renderPass2.clear = false;
+
+    const copyShder = new ShaderPass(CopyShader);
+    copyShder.renderToScreen = true;
+
+    // const bloom = new UnrealBloomPass(new THREE.Vector2( w, h ), 1.5, 0.4, 0.85);
+    const bloom = new ShaderPass(ColorifyShader);
+    bloom.renderToScreen = true;
+
+    const maskBloom = new MaskPass(sceneSun, camera);
+    const maskDot = new MaskPass(scene, camera);
 
 
+    const clearMask = new ClearMaskPass();
+    
+    effectComposer = new EffectComposer(renderer);
+    effectComposer.renderTarget1.stencilBuffer = true;
+    effectComposer.renderTarget2.stencilBuffer = true;
+
+    const dotEffect = new DotScreenPass();
+
+    effectComposer.addPass(randerPass);
+    effectComposer.addPass(renderPass2);
+
+    effectComposer.addPass(maskBloom);
+    effectComposer.addPass(bloom);
+    effectComposer.addPass(clearMask);
+
+    effectComposer.addPass(maskDot);
+    effectComposer.addPass(dotEffect);
+    effectComposer.addPass(clearMask);
 
 
+    effectComposer.addPass(copyShder);
 
-
-    // scene.add(B);
+   
 
 
 
@@ -195,11 +144,13 @@ function main() {
 main();
 
 function tick(time) {
+    renderer.autoClear = false;
     stats.update();
-
-    allObjects.material.uniforms.u_time.value = time;
+    obj.rotation.y += 0.005;
+    obj.rotation.x += 0.01;
+    // allObjects.material.uniforms.u_time.value = time;
     // allObjects.geometry.verticesNeedUpdate = true;
-    renderer.render(scene, camera);
+    effectComposer.render();
     window.requestAnimationFrame(tick);
 }
 
